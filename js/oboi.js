d28f11e1de06e4b9f11c65c928fe0d8f489fd57d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-15 06:17:02 +0000

    Deploy rustamSurmashev/Oboi to github.com/rustamSurmashev/Oboi.git:gh-pages

---
+++ js/oboi.js
@@ -0,0 +1,14 @@
+export function calculateOboi(roomPerimetr, roomHeight, oboiWidth, oboiLength) {
+    let sizePerimetr = roomPerimetr;
+    let sizeHeidht = roomHeight + 10;
+    let sizeOboiWidth = oboiWidth;
+    let sizeOboiLenght = oboiLength;
+
+    let polosaOboi = sizePerimetr/sizeOboiWidth;
+    let dlinaOboi = sizeHeidht/sizeOboiLenght;
+
+    let result = polosaOboi/dlinaOboi;
+
+    return Math.ceil(result);
+}
+
